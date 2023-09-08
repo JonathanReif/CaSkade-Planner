aa5d66be1321455e4bd05178bdbf1210b0dd0c0a
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-08 15:13:11 +0200

    Fixed problem with overwriting existing happening.

--- smt-planning/dicts/CapabilityDictionary.py
+++ smt-planning/dicts/CapabilityDictionary.py
@@ -6,10 +6,12 @@ class CapabilityDictionary:
 	def __init__(self):
 		self.capabilities: Dict[str, Dict[int, BoolRef]] = dict()
 
-	def addEntry(self, iri:str, happening: int):
+	def addCapability(self, iri:URIRef):
+		self.capabilities[str(iri)] = {}
+
+	def addCapabilityHappening(self, iri:URIRef, happening: int):
 		variableName = str(iri) + "_" + str(happening)
-		self.capabilities[iri] = {}
-		self.capabilities[iri][happening] = Bool(variableName)
+		self.capabilities[str(iri)][happening] = Bool(variableName)
 
 	def getCapabilityVariableByIriAndHappening(self, iri: URIRef, happening:int) -> BoolRef:
 		return self.capabilities[str(iri)][happening]
