8aade8da39a2e5d26fc7d3f4d165b505c0778e11
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-21 09:23:56 +0200

    Fixed query and added boolean variables

--- smt-planning/capability_effects.py
+++ smt-planning/capability_effects.py
@@ -3,7 +3,7 @@ from dicts.CapabilityDictionary import CapabilityDictionary
 from dicts.PropertyDictionary import PropertyDictionary
 from typing import List
 from openmath.parse_openmath import from_open_math_in_graph
-from z3 import Implies
+from z3 import Implies, Not
 
 
 def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictionary, property_dictionary: PropertyDictionary, happenings: int, event_bound: int) -> List:
@@ -17,7 +17,7 @@ def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictiona
 	PREFIX DINEN61360: <http://www.hsu-ifa.de/ontologies/DINEN61360#>
 	PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
 
-	select ?cap ?de ?prop where {  
+	select ?cap ?de ?log ?val where {  
 		?cap a cask:ProvidedCapability;
 			^CSS:requiresCapability ?process.
 		?process VDI3682:hasOutput ?output.
@@ -36,21 +36,32 @@ def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictiona
 			effect_property = property_dictionary.getPropertyVariable(row.de, happening, 1)							# type: ignore
 			relation = str(row.log)																					# type: ignore
 			value = str(row.val)																					# type: ignore		
-			match relation:
-				case "<":
-					effect = Implies(current_capability, effect_property < value)									# type: ignore
-				case "<=":
-					effect = Implies(current_capability, effect_property <= value)									# type: ignore
-				case "=":
-					effect = Implies(current_capability, effect_property == value)									# type: ignore
-				case "!=":
-					effect = Implies(current_capability, effect_property != value)									# type: ignore
-				case ">=":
-					effect = Implies(current_capability, effect_property >= value)									# type: ignore
-				case ">":
-					effect = Implies(current_capability, effect_property > value)									# type: ignore
-				case _:
-					raise RuntimeError("Incorrect logical relation")
-			effects.append(effect)
-	
+			prop_type = property_dictionary.getPropertyType(row.de) 												# type: ignore
+			if prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Real":
+				match relation:
+					case "<":
+						effect = Implies(current_capability, effect_property < value)									# type: ignore
+					case "<=":
+						effect = Implies(current_capability, effect_property <= value)									# type: ignore
+					case "=":
+						effect = Implies(current_capability, effect_property == value)									# type: ignore
+					case "!=":
+						effect = Implies(current_capability, effect_property != value)									# type: ignore
+					case ">=":
+						effect = Implies(current_capability, effect_property >= value)									# type: ignore
+					case ">":
+						effect = Implies(current_capability, effect_property > value)									# type: ignore
+					case _:
+						raise RuntimeError("Incorrect logical relation")
+				effects.append(effect)
+			elif prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Boolean":
+				match value: 
+					case 'true':
+						effect = Implies(current_capability, effect_property)
+					case 'false':
+						effect = Implies(current_capability, Not(effect_property))
+					case _:
+						raise RuntimeError("Incorrect value for Boolean")
+				effects.append(effect)
+				
 	return effects
