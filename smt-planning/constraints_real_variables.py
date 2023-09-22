a582a2f266ad4165486953b7446088d3f1ff01c9
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-22 17:28:27 +0200

    AND link in the constraint real variables if multiple capabilities have influence on property.

--- smt-planning/constraints_real_variables.py
+++ smt-planning/constraints_real_variables.py
@@ -1,5 +1,5 @@
 from rdflib import Graph
-from z3 import Implies, Not, Or
+from z3 import Implies, Not, And
 from typing import List
 
 from dicts.CapabilityDictionary import CapabilityDictionary
@@ -13,14 +13,15 @@ def get_variable_constraints(graph: Graph, capability_dict: CapabilityDictionary
         PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
         PREFIX CaSk: <http://www.w3id.org/hsu-aut/cask#>
         PREFIX VDI3682: <http://www.w3id.org/hsu-aut/VDI3682#>
-        SELECT ?cap ?de WHERE { 
+        SELECT ?de (GROUP_CONCAT(?cap; separator=", ") as ?caps) WHERE { 
             ?cap a CaSk:ProvidedCapability;
                 ^CSS:requiresCapability ?process.
             ?process VDI3682:hasOutput ?out.
             ?out VDI3682:isCharacterizedBy ?id.
             ?id ^DINEN61360:has_Instance_Description ?de;
                 a DINEN61360:Real.
-        } """
+        }
+        GROUP by ?de"""
     
     # get all properties that are not influenced by capability effect
     query_props_not_effected = """
@@ -46,10 +47,15 @@ def get_variable_constraints(graph: Graph, capability_dict: CapabilityDictionary
     constraints = []
     for happening in range(happenings):
         for row in results:
-            currentCap = capability_dict.getCapabilityVariableByIriAndHappening(row.cap, happening) # type: ignore
+            caps_result = row.caps.split(', ')           # type: ignore 
+            caps = []
+            for cap in caps_result:                    # type: ignore
+                currentCap = capability_dict.getCapabilityVariableByIriAndHappening(cap, happening) # type: ignore
+                caps.append(currentCap)
             prop_start = property_dictionary.getPropertyVariable(row.de, happening, 0) # type: ignore
             prop_end = property_dictionary.getPropertyVariable(row.de, happening, 1) # type: ignore
-            constraint = Implies(Not(currentCap), prop_end == prop_start)
+            caps_constraint = [Not(cap) for cap in caps]                
+            constraint = Implies(And(*caps_constraint), prop_end == prop_start)
             constraints.append(constraint)
             
             
