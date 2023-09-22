de71d27d195d96ecf14e9506b7ac36fdf3dcc4c1
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2023-09-22 13:53:40 +0200

    adds type check to add syntax-correct constraints

--- smt-planning/init.py
+++ smt-planning/init.py
@@ -1,50 +1,62 @@
 from rdflib import Graph
+from z3 import Not
 from dicts.PropertyDictionary import PropertyDictionary
 
 def get_init(graph: Graph, property_dictionary: PropertyDictionary):
-    
-    '''
-    Get initial state properties
-    Check for Outputs because resource can have actual value for required output (e.g. longitude)
-    '''
-    sparql_string = """
-        PREFIX DINEN61360: <http://www.hsu-ifa.de/ontologies/DINEN61360#>
-        PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
-        PREFIX CaSk: <http://www.w3id.org/hsu-aut/cask#>
-        PREFIX VDI3682: <http://www.w3id.org/hsu-aut/VDI3682#>
-        SELECT DISTINCT ?de ?log ?val WHERE { 
-            ?cap a CaSk:RequiredCapability;
-            ^CSS:requiresCapability ?process.
-            ?process VDI3682:hasInput|VDI3682:hasOutput ?inout.
-            ?inout VDI3682:isCharacterizedBy ?id.
-            ?de DINEN61360:has_Instance_Description ?id.
-            ?de DINEN61360:has_Instance_Description ?id_any.
-            ?id_any DINEN61360:Expression_Goal "Actual_Value";
-                DINEN61360:Logic_Interpretation ?log;
-                DINEN61360:Value ?val. 
-        } """
+	
+	'''
+	Get initial state properties by checking the values of all Actual_Value instances
+	'''
+	sparql_string = """
+		PREFIX DINEN61360: <http://www.hsu-ifa.de/ontologies/DINEN61360#>
+		PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
+		PREFIX CaSk: <http://www.w3id.org/hsu-aut/cask#>
+		PREFIX VDI3682: <http://www.w3id.org/hsu-aut/VDI3682#>
+		SELECT DISTINCT ?de ?log ?val WHERE { 
+			?de a DINEN61360:Data_Element.
+			?de DINEN61360:has_Instance_Description ?id.
+			?id DINEN61360:Expression_Goal "Actual_Value";
+				DINEN61360:Logic_Interpretation ?log;
+				DINEN61360:Value ?val. 
+		} """
 
-    # Inits 
-    results = graph.query(sparql_string)
-    inits = []
-    for row in results:
-        property = property_dictionary.getPropertyVariable(row.de, 0, 0)				# type: ignore
-        relation = str(row.log)															# type: ignore
-        value = str(row.val)														    # type: ignore
-        match relation:
-            case "<":
-                init = property < value
-            case "<=":
-                init = property <= value
-            case "=":
-                init = property == value
-            case "!=":
-                init = property != value
-            case ">=":
-                init = property >= value
-            case ">":
-                init = property > value
-            case _:
-                raise RuntimeError("Incorrent logical relation")
-        inits.append(init)	
-    return inits										
+	# Inits 
+	results = graph.query(sparql_string)
+	inits = []
+	for row in results:
+		property = property_dictionary.getPropertyVariable(row.de, 0, 0)				# type: ignore
+		relation = str(row.log)															# type: ignore
+		value = str(row.val)														    # type: ignore
+		
+		prop_type = property_dictionary.getPropertyType(row.de) # type: ignore
+		if prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Real":
+
+			match relation:
+				case "<":
+					init = property < value
+				case "<=":
+					init = property <= value
+				case "=":
+					init = property == value
+				case "!=":
+					init = property != value
+				case ">=":
+					init = property >= value
+				case ">":
+					init = property > value
+				case _:
+					raise RuntimeError("Incorrent logical relation")
+			inits.append(init)
+
+		elif prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Boolean":
+			match value: 
+				case 'true':
+					init = property
+				case 'false':
+					init = Not(property)
+				case _:
+					raise RuntimeError("Incorrect value for Boolean")
+				
+			inits.append(init)
+	
+	return inits										
