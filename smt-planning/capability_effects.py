0981269fd88d33991cbe00141525c7b0f97e092f
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-25 01:48:50 +0200

    Capability effect changes every related property.

--- smt-planning/capability_effects.py
+++ smt-planning/capability_effects.py
@@ -2,8 +2,8 @@ from rdflib import Graph
 from dicts.CapabilityDictionary import CapabilityDictionary
 from dicts.PropertyDictionary import PropertyDictionary
 from typing import List
-from openmath.parse_openmath import from_open_math_in_graph
-from z3 import Implies, Not
+from property_links import get_related_properties_at_same_time
+from z3 import Implies, Not, BoolRef, ArithRef
 
 
 def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictionary, property_dictionary: PropertyDictionary, happenings: int, event_bound: int) -> List:
@@ -33,35 +33,46 @@ def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictiona
 	for happening in range(happenings):
 		for row in results:
 			current_capability = capability_dictionary.getCapabilityVariableByIriAndHappening(row.cap, happening)	# type: ignore
-			effect_property = property_dictionary.get_provided_property(row.de, happening, 1)							# type: ignore
+			effect_property = property_dictionary.get_provided_property(row.de, happening, 1)						# type: ignore
 			relation = str(row.log)																					# type: ignore
 			value = str(row.val)																					# type: ignore		
 			prop_type = property_dictionary.get_property_type(row.de) 												# type: ignore
-			if prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Real":
-				match relation:
-					case "<":
-						effect = Implies(current_capability, effect_property < value)									# type: ignore
-					case "<=":
-						effect = Implies(current_capability, effect_property <= value)									# type: ignore
-					case "=":
-						effect = Implies(current_capability, effect_property == value)									# type: ignore
-					case "!=":
-						effect = Implies(current_capability, effect_property != value)									# type: ignore
-					case ">=":
-						effect = Implies(current_capability, effect_property >= value)									# type: ignore
-					case ">":
-						effect = Implies(current_capability, effect_property > value)									# type: ignore
-					case _:
-						raise RuntimeError("Incorrect logical relation")
-				effects.append(effect)
-			elif prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Boolean":
-				match value: 
-					case 'true':
-						effect = Implies(current_capability, effect_property)
-					case 'false':
-						effect = Implies(current_capability, Not(effect_property))
-					case _:
-						raise RuntimeError("Incorrect value for Boolean")
+			effect = generate_effect_constraint(current_capability, effect_property, prop_type, relation, value)	
+			effects.append(effect)
+
+			related_properties = get_related_properties_at_same_time(graph, property_dictionary, str(row.de), happening, 1) # type: ignore 
+			for related_property in related_properties:
+				effect = generate_effect_constraint(current_capability, related_property, prop_type, relation, value)
 				effects.append(effect)
 				
 	return effects
+
+def generate_effect_constraint(capability: BoolRef, property: BoolRef | ArithRef, prop_type: str, relation: str, value: str) -> BoolRef :	# type: ignore
+	
+	if prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Real":
+		match relation:
+			case "<":
+				effect = Implies(capability, property < value)									# type: ignore
+			case "<=":
+				effect = Implies(capability, property <= value)									# type: ignore
+			case "=":
+				effect = Implies(capability, property == value)									# type: ignore
+			case "!=":
+				effect = Implies(capability, property != value)									# type: ignore
+			case ">=":
+				effect = Implies(capability, property >= value)									# type: ignore
+			case ">":
+				effect = Implies(capability, property > value)									# type: ignore
+			case _:
+				raise RuntimeError("Incorrect logical relation")
+		return effect
+	
+	elif prop_type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Boolean":
+		match value: 
+			case 'true':
+				effect = Implies(capability, property)
+			case 'false':
+				effect = Implies(capability, Not(property))
+			case _:
+				raise RuntimeError("Incorrect value for Boolean")
+		return effect
