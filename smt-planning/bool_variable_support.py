f0979147b744684d1f06b66cb0577b690f373c39
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-25 00:03:53 +0200

    Proposition support and real variable continuous change were reversed. Only update over previous value.

--- smt-planning/bool_variable_support.py
+++ smt-planning/bool_variable_support.py
@@ -1,38 +1,25 @@
 from rdflib import Graph
-from dicts.CapabilityDictionary import CapabilityDictionary
 from dicts.PropertyDictionary import PropertyDictionary
-from property_links import get_related_properties_at_same_time
 from typing import List
-from z3 import Implies, Not, Or
+from z3 import Implies, Not
 
 def getPropositionSupports(graph: Graph, property_dictionary: PropertyDictionary, happenings: int, event_bound: int) -> List:
 	supports = []
 	for happening in range(happenings)[1:]:
 		for property_item in property_dictionary.provided_properties.items():
-			property_iri = property_item[0]
 			property = property_item[1]
 			if property.type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Boolean":
 				property_current_happening_start = property.states[happening][0]
 				property_last_happening_end = property.states[happening-1][event_bound-1]
-				
-				if property.relation_type == "Output": 
-					support = Implies(property_current_happening_start, property_last_happening_end)
-					supports.append(support)
-
-					support_negated = Implies(Not(property_current_happening_start), Not(property_last_happening_end))
-					supports.append(support_negated)
-					continue
 
 				# Track change between happenings, so that no random change is possible
 				# TODO: Why do we need both implies (positive and negative)? Isn't this the same as setting start and last_end equal?
-				# 1: If a property is set at start of a happening, it must have been set at the last happening's end (either same or related property)
-				related_properties_last_happening_end = get_related_properties_at_same_time(graph, property_dictionary, property_iri, happening-1, event_bound-1)
-				support = Implies(property_current_happening_start, Or(property_last_happening_end, *related_properties_last_happening_end))
+				# 1: If a property is set at start of a happening, it must have been set at the last happening's end 
+				support = Implies(property_current_happening_start, property_last_happening_end)
 				supports.append(support)
 				
-				# 1: If a property is NOT set at start of a happening, it must NOT have been set at the last happening's end (either same or related property)
-				nots = [Not(x) for x in related_properties_last_happening_end]
-				support_negated = Implies(Not(property_current_happening_start), Or(Not(property_last_happening_end), *nots))
+				# 1: If a property is NOT set at start of a happening, it must NOT have been set at the last happening's end 
+				support_negated = Implies(Not(property_current_happening_start), Not(property_last_happening_end))
 				supports.append(support_negated)
 
 	return supports
--- smt-planning/real_variable_contin_change.py
+++ smt-planning/real_variable_contin_change.py
@@ -1,28 +1,17 @@
 from dicts.PropertyDictionary import PropertyDictionary
 from typing import List
 from rdflib import Graph
-from z3 import Or
-from property_links import get_related_properties_at_same_time
 
 def get_real_variable_continuous_changes(graph: Graph, property_dictionary: PropertyDictionary, happenings: int, event_bound: int) -> List:
 	continuous_changes = []
 	for happening in range(happenings)[1:]:
 		for property_item in property_dictionary.provided_properties.items():
-			property_iri = property_item[0]
 			property = property_item[1]
 			if property.type == "http://www.hsu-ifa.de/ontologies/DINEN61360#Real":
 				property_current_happening_start = property.states[happening][0]
 				property_last_happening_end = property.states[happening-1][event_bound-1]
-
-				if property.relation_type == "Output": 
-					continuous_change = property_current_happening_start == property_last_happening_end
-					continuous_changes.append(continuous_change)
-					continue
 				
-				related_properties_last_happening_end = get_related_properties_at_same_time(graph, property_dictionary, property_iri, happening-1, event_bound-1)
-
-				# Track change between happenings, so that no random change is possible (for both the same and related properties)
-				related_equations = [property_current_happening_start == x for x in related_properties_last_happening_end]
-				continuous_change = Or(property_current_happening_start == property_last_happening_end, *related_equations)
+				# Track change between happenings, so that no random change is possible
+				continuous_change = property_current_happening_start == property_last_happening_end
 				continuous_changes.append(continuous_change)
 	return continuous_changes
