1eabf50665ab77e579233839315c32e5ae92f5f9
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-25 16:29:49 +0200

    In capability effect, we equate the related properties with the changed property. We do the same for the capability constraints.

--- smt-planning/capability_effects.py
+++ smt-planning/capability_effects.py
@@ -23,26 +23,31 @@ def getCapabilityEffects(graph: Graph, capability_dictionary: CapabilityDictiona
 		?process VDI3682:hasOutput ?output.
 		?output VDI3682:isCharacterizedBy ?id.
 		?id DINEN61360:Expression_Goal "Assurance";
-			DINEN61360:Logic_Interpretation ?log;
-			DINEN61360:Value ?val.
+			DINEN61360:Logic_Interpretation ?log.
 		?de DINEN61360:has_Instance_Description ?id.
+		OPTIONAL { ?id DINEN61360:Value ?val. }
 	} """
 
 	results = graph.query(query_string)
 	effects = []
 	for happening in range(happenings):
 		for row in results:
-			current_capability = capability_dictionary.getCapabilityVariableByIriAndHappening(row.cap, happening)	# type: ignore
-			effect_property = property_dictionary.get_provided_property(row.de, happening, 1)						# type: ignore
-			relation = str(row.log)																					# type: ignore
-			value = str(row.val)																					# type: ignore		
-			prop_type = property_dictionary.get_property_type(row.de) 												# type: ignore
-			effect = generate_effect_constraint(current_capability, effect_property, prop_type, relation, value)	
-			effects.append(effect)
-
+			current_capability = capability_dictionary.getCapabilityVariableByIriAndHappening(row.cap, happening)		# type: ignore
+			effect_property = property_dictionary.get_provided_property(row.de, happening, 1)							# type: ignore
+			relation = str(row.log)																						# type: ignore
+			# Case 1: Constant effect 																					# type: ignore
+			if row.val: 																								# type: ignore
+				value = str(row.val)																					# type: ignore		
+				prop_type = property_dictionary.get_property_type(row.de) 												# type: ignore
+				effect = generate_effect_constraint(current_capability, effect_property, prop_type, relation, value)	
+				effects.append(effect)
+			else: 
+				# TODO: Constraint effect currently in capability_constraints. Needs to be changed  
+				pass
 			related_properties = get_related_properties_at_same_time(graph, property_dictionary, str(row.de), happening, 1) # type: ignore 
 			for related_property in related_properties:
-				effect = generate_effect_constraint(current_capability, related_property, prop_type, relation, value)
+				# effect = generate_effect_constraint(current_capability, related_property, prop_type, relation, value)
+				effect = Implies(current_capability, effect_property == related_property)
 				effects.append(effect)
 				
 	return effects
