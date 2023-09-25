b24535bcb08c4b75429b6cafca1ecec5d514cd47
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-25 16:35:05 +0200

    For boolean properties, all capabilities and related capabilities that directly or indirectly modify this property are added.

--- smt-planning/constraints_bools.py
+++ smt-planning/constraints_bools.py
@@ -4,6 +4,7 @@ from typing import List
 
 from dicts.CapabilityDictionary import CapabilityDictionary
 from dicts.PropertyDictionary import PropertyDictionary
+from property_links import get_related_capabilities_at_same_time_bool
 
 def get_bool_constraints(graph: Graph, capability_dict: CapabilityDictionary, property_dictionary: PropertyDictionary, happenings: int, event_bound: int) -> List:
     
@@ -31,17 +32,25 @@ def get_bool_constraints(graph: Graph, capability_dict: CapabilityDictionary, pr
         PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
         PREFIX CaSk: <http://www.w3id.org/hsu-aut/cask#>
         PREFIX VDI3682: <http://www.w3id.org/hsu-aut/VDI3682#>
-        SELECT ?de WHERE { 
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT ?de ?cap ?val WHERE { 
             ?cap a CaSk:ProvidedCapability;
                 ^CSS:requiresCapability ?process.
             ?process VDI3682:hasInput ?in.
             ?in VDI3682:isCharacterizedBy ?id.
             ?id ^DINEN61360:has_Instance_Description ?de;
-                a DINEN61360:Boolean.
+                a DINEN61360:Boolean. 
+            ?de DINEN61360:has_Type_Description ?td. 
+            ?process VDI3682:hasOutput ?out.
+            ?out VDI3682:isCharacterizedBy ?out_id.
+            ?out_id ^DINEN61360:has_Instance_Description ?out_de;
+                a DINEN61360:Boolean;
+                DINEN61360:Value ?val. 
+            ?out_de DINEN61360:has_Type_Description ?td. 
             FILTER NOT EXISTS {
-                ?process VDI3682:hasOutput ?out.
-                ?out VDI3682:isCharacterizedBy ?out_id.
-                ?out_id ^DINEN61360:has_Instance_Description ?de.
+                ?process VDI3682:hasOutput ?out2.
+                ?out2 VDI3682:isCharacterizedBy ?out2_id.
+                ?out2_id ^DINEN61360:has_Instance_Description ?de.
             }
         } """
 	
@@ -52,6 +61,8 @@ def get_bool_constraints(graph: Graph, capability_dict: CapabilityDictionary, pr
         previous_value = None
         previous_prop_start = None
         previous_prop_end = None
+        previous_rel_pos = []
+        previous_rel_neg = []
         i = 0
         number_of_rows = len(results)
         for row in results:
@@ -59,51 +70,77 @@ def get_bool_constraints(graph: Graph, capability_dict: CapabilityDictionary, pr
             value = str(row.val)                                                        # type: ignore
             caps_result = row.caps.split(', ')                                          # type: ignore 
             caps = []
+            rel_caps_pos = []
+            rel_caps_neg = []
             for cap in caps_result:                                                     # type: ignore
                 currentCap = capability_dict.getCapabilityVariableByIriAndHappening(cap, happening) # type: ignore
                 caps.append(currentCap)
+                related_caps = get_related_capabilities_at_same_time_bool(graph, capability_dict, cap, str(row.de), happening) # type: ignore
+                for related_cap in related_caps:
+                    if related_cap in caps or related_caps[related_cap] == "true": 
+                        rel_caps_pos.append(related_cap)
+                    elif related_cap in caps or related_caps[related_cap] == "false": 
+                        rel_caps_neg.append(related_cap)   
+                
             prop_start = property_dictionary.get_provided_property(row.de, happening, 0) # type: ignore
             prop_end = property_dictionary.get_provided_property(row.de, happening, 1) # type: ignore
             
             if value == "true":
-                constraint_1 = Implies(prop_end, Or(prop_start, *caps))
+                caps_sum = caps + rel_caps_pos
+                constraint_1 = Implies(prop_end, Or(prop_start, *caps_sum))
                 constraints.append(constraint_1)
             elif value == "false":
-                caps_constraint = [Not(cap) for cap in caps] 
-                constraint_2 = Implies(Not(prop_end), Or(Not(prop_start), *caps_constraint))
+                caps_sum = caps + rel_caps_neg
+                constraint_2 = Implies(Not(prop_end), Or(Not(prop_start), *caps_sum))
                 constraints.append(constraint_2)
 
             if previous_property == row.de: continue                                       # type: ignore
             if previous_value == "true":
-                constraint_2 = Implies(Not(previous_prop_end), Not(previous_prop_start))
+                constraint_2 = Implies(Not(previous_prop_end), Or(Not(previous_prop_start), *previous_rel_neg))
                 constraints.append(constraint_2)
 
             elif previous_value == "false":
-                constraint_1 = Implies(previous_prop_end, previous_prop_start)
+                constraint_1 = Implies(previous_prop_end, Or(previous_prop_start, *previous_rel_pos))
                 constraints.append(constraint_1)
             
             if i == number_of_rows:
                 if value == "true":
-                    constraint_2 = Implies(Not(prop_end), Not(prop_start))
+                    constraint_2 = Implies(Not(prop_end), Or(Not(prop_start), *rel_caps_neg))
                     constraints.append(constraint_2)
 
                 elif value == "false":
-                    constraint_1 = Implies(prop_end, prop_start)
+                    constraint_1 = Implies(prop_end, Or(prop_start, *rel_caps_pos))
                     constraints.append(constraint_1)
 
             previous_property = row.de                                                   # type: ignore 
             previous_value = str(row.val)                                                # type: ignore
             previous_prop_end = prop_end
             previous_prop_start = prop_start
+            previous_rel_pos = rel_caps_pos
+            previous_rel_neg = rel_caps_neg
+
             
 
     results = graph.query(query_props_not_effected) 
     for happening in range(happenings):
         for row in results:
+            rel_caps_pos = []
+            rel_caps_neg = []
+            currentCap = capability_dict.getCapabilityVariableByIriAndHappening(row.cap, happening) # type: ignore
+            if str(row.val) == "true":                                                              # type: ignore
+                rel_caps_pos.append(currentCap)
+            elif str(row.val) == "false":                                                           # type: ignore
+                rel_caps_neg.append(currentCap)
+            related_caps = get_related_capabilities_at_same_time_bool(graph, capability_dict, str(row.cap), str(row.de), happening) # type: ignore
+            for related_cap in related_caps:
+                    if related_caps[related_cap] == "true": 
+                        rel_caps_pos.append(related_cap)
+                    elif related_caps[related_cap] == "false": 
+                        rel_caps_neg.append(related_cap)   
             prop_start = property_dictionary.get_provided_property(row.de, happening, 0) # type: ignore
             prop_end = property_dictionary.get_provided_property(row.de, happening, 1) # type: ignore
-            constraint_1 = Implies(prop_end, prop_start)
-            constraint_2 = Implies(Not(prop_end), Not(prop_start))
+            constraint_1 = Implies(prop_end, Or(prop_start, *rel_caps_pos))
+            constraint_2 = Implies(Not(prop_end), Or(Not(prop_start), *rel_caps_neg))
             constraints.append(constraint_1)
             constraints.append(constraint_2)
 
