a0d1bedea3e0f853965e088c05250244f5b57a3e
Miguel Vieira <miguel_vieira@gmx.de>
2023-09-26 23:30:25 +0200

    Inital mutexes for all capabilities of same resource.

---
+++ smt-planning/capability_mutexes.py
@@ -0,0 +1,35 @@
+from rdflib import Graph
+from z3 import Not, Or
+import itertools
+from typing import List
+from dicts.CapabilityDictionary import CapabilityDictionary, Capability
+
+def get_capability_mutexes(graph: Graph, capability_dictionary: CapabilityDictionary, happenings):
+
+    query_string = """
+        PREFIX CaSk: <http://www.w3id.org/hsu-aut/cask#>
+        PREFIX CSS: <http://www.w3id.org/hsu-aut/css#>
+
+        SELECT ?res (GROUP_CONCAT(?cap; separator=", ") as ?caps) WHERE {  
+            ?res CSS:providesCapability ?cap. 
+            ?cap a CaSk:ProvidedCapability.
+        } GROUP BY ?res """
+    result = graph.query(query_string)
+
+    constraints = []
+
+    for row in result: 
+        caps = set(row.caps.split(", "))
+        capabilities: List[Capability] = []
+        for cap in caps:
+            capability = capability_dictionary.get_capability(cap)
+            capabilities.append(capability)
+
+        combinations = list(itertools.combinations(capabilities, 2))
+
+        for happening in range(happenings):
+            for combination in combinations:
+                constraint = Or(Not(combination[0].occurrences[happening].z3_variable), Not(combination[1].occurrences[happening].z3_variable))
+                constraints.append(constraint)
+
+    return constraints
