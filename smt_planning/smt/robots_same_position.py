1a2b289bf8cff126b64567a965d8536ef8ec2ac9
Miguel Vieira <miguel_vieira@gmx.de>
2024-06-11 08:32:37 +0200

    fixed constraints for robots not allowed to be at same position

--- smt_planning/smt/robots_same_position.py
+++ smt_planning/smt/robots_same_position.py
@@ -11,16 +11,20 @@ def robot_positions_smt(happenings: int, event_bound: int):
     lat_props = []
     long_props = []
 
+    for robot in geofence_dictionary.robots.values():
+        lat_props.append(robot.latitude.iri)
+        long_props.append(robot.longitude.iri)
+    
     for happening in range(happenings):
         for event in range(event_bound):
-            for robot in geofence_dictionary.robots.values():
-                lat1 = property_dictionary.get_provided_property_occurrence(robot.latitude.iri, happening, event).z3_variable
-                long1 = property_dictionary.get_provided_property_occurrence(robot.longitude.iri, happening, event).z3_variable
-                lat_props.append(lat1)
-                long_props.append(long1)
-
             for i in range(len(lat_props) - 1): 
-                robots_not_same_position = Or(lat_props[i] != lat_props[i+1], long_props[i] != long_props[i+1])
+                lat_0 = property_dictionary.get_provided_property_occurrence(lat_props[i], happening, event).z3_variable
+                long_0 = property_dictionary.get_provided_property_occurrence(long_props[i], happening, event).z3_variable
+                for j in range(i + 1, len(lat_props)):
+                    lat_1 = property_dictionary.get_provided_property_occurrence(lat_props[j], happening, event).z3_variable
+                    long_1 = property_dictionary.get_provided_property_occurrence(long_props[j], happening, event).z3_variable
+
+                robots_not_same_position = Or(lat_0 != lat_1, long_0 != long_1)
                 robots_not_same_position_constraints.append(robots_not_same_position)
 
     return robots_not_same_position_constraints
