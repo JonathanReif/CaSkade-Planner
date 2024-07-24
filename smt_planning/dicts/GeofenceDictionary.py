eddb73f105783bdef8c639714a9f77697ec33438
Miguel Vieira <miguel_vieira@gmx.de>
2024-07-24 13:31:38 +0200

    New class gozone to put ontology information into object

--- smt_planning/dicts/GeofenceDictionary.py
+++ smt_planning/dicts/GeofenceDictionary.py
@@ -7,7 +7,7 @@ class Point:
         self.longitude: float
         self.latitude: float
 
-class Geofence: 
+class Area: 
     def __init__(self, iri: str):
         self.iri = iri
         self.points: Dict[str, Point] = {} 
@@ -20,16 +20,41 @@ class Geofence:
         elif longitude is not None:
             point.longitude = longitude
 
-    def generate_geofence_polygon(self) -> List[Tuple[float, float]]:
-        geofence_polygon = []
+    def generate_polygon(self) -> List[Tuple[float, float]]:
+        polygon = []
         for point in self.points.values():
-            geofence_polygon.append((point.longitude, point.latitude))
-        return geofence_polygon
+            polygon.append((point.longitude, point.latitude))
+        return polygon
     
+class Geofence(Area):
+    def __init__(self, iri: str):
+        super().__init__(iri)
+
+class Gozone(Area): 
+    def __init__(self, iri: str):
+        super().__init__(iri)
+        self.holes: Dict[str, Area] = {}
+        self.alternative_gozones: Dict[str, Area] = {}
+
+    def add_hole(self, iri: str):
+        hole = Area(iri)
+        self.holes.setdefault(iri, hole)
+
+    def add_alternative_gozone(self, iri: str):
+        alt_gozone = Area(iri)
+        self.alternative_gozones.setdefault(iri, alt_gozone)
+    
+    def add_hole_point(self, hole_iri: str, point_iri: str, latitude: float = None, longitude: float = None):   # type: ignore
+        self.holes[hole_iri].add_point(point_iri, latitude, longitude)
+
+    def add_alternative_gozone_point(self, alt_gozone_iri: str, point_iri: str, latitude: float = None, longitude: float = None): # type: ignore
+        self.alternative_gozones[alt_gozone_iri].add_point(point_iri, latitude, longitude)
+
 class Robot: 
     def __init__(self, iri: str):
         self.iri = iri
         self.geofence = Geofence(self.iri + "/geofence")
+        self.gozone = Gozone(self.iri + "/gozone") 
         self.longitude: Property
         self.latitude: Property
 
@@ -38,6 +63,13 @@ class Robot:
             self.longitude = property
         else:
             self.latitude = property
+    
+    def get_iri(self) -> str:
+        return self.iri
+    
+    def get_gozone(self) -> Gozone:
+        return self.gozone
+    
 
 class RobotGeofenceDictionary:
     def __init__(self):
@@ -48,3 +80,6 @@ class RobotGeofenceDictionary:
 
     def add_geofence_point(self, robot_iri: str, point_iri: str, latitude: float = None, longitude: float = None):  # type: ignore
         self.robots[robot_iri].geofence.add_point(point_iri, latitude, longitude)
+
+    def get_robots(self) -> Dict[str, Robot]:
+        return self.robots
