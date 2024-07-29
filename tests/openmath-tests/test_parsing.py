5ce449b2cd0ef9e6ff84d5174b4194c9b6a846be
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2024-07-29 13:17:04 +0200

    adds tests for openmath parsing

--- tests/openmath-tests/test_parsing.py
+++ tests/openmath-tests/test_parsing.py
@@ -2,25 +2,36 @@ import os
 
 from smt_planning.openmath.parse_openmath import from_open_math_in_graph
 from smt_planning.ontology_handling.query_handlers import FileQueryHandler
+from smt_planning.smt.capability_constraints import infix_to_prefix
 
-class TestThreeCaps:
+class TestParsing:
 	
-	def test_create_and_solve(self):
-		# Relativer Pfad
+	def test_simple_equation(self):
+		ontology_file = os.path.join(os.getcwd(), "tests", "openmath-tests", "simple_equation.ttl")
+		query_handler = FileQueryHandler(ontology_file)
+		equation = from_open_math_in_graph(query_handler, "http://example.org/ontology#myApplication_equals", 1, 1)
+		expected_equation = "|http://example.org/ontology#x_1_1| = |http://example.org/ontology#y_1_1|"
+		assert expected_equation == equation
+		return equation
+	
+	def test_conversion_to_infix(self):
+		infix_expression = self.test_simple_equation()
+		prefix_expression = infix_to_prefix(infix_expression)
+		expected_expression = ' = |http://example.org/ontology#x_1_1| |http://example.org/ontology#y_1_1|'
+		assert prefix_expression == expected_expression
+
+
+	def test_transport(self):
+		ontology_file = os.path.join(os.getcwd(), "tests", "openmath-tests", "transport.ttl")
+		query_handler = FileQueryHandler(ontology_file)
+		equation = from_open_math_in_graph(query_handler, "http://www.hsu-hh.de/aut/ontologies/lab/MPS500/Transport#TargetStationConstraint", 1, 1)
+		expected_equation = "|http://www.hsu-hh.de/aut/ontologies/lab/MPS500/Transport#ProductAtTargetStation_StationID_DE_1_1| = |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/Transport#TargetStation_DE_1_1|"
+		assert expected_equation == equation
+
+
+	def test_cylinder_supply(self):
 		ontology_file = os.path.join(os.getcwd(), "tests", "openmath-tests", "cylinder-supply.ttl")
-		# ontology_file = os.path.join(os.getcwd(), "tests", "riva_one_rover.ttl")
-
-		# Absoluter Pfad
-		absolute_ontology_file = os.path.abspath(ontology_file)
-
-		# Convert to file URI
-		file_uri = absolute_ontology_file.replace("\\", "/")
-		query_handler = FileQueryHandler(file_uri)
+		query_handler = FileQueryHandler(ontology_file)
 		equation = from_open_math_in_graph(query_handler, "http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#ColorStackConstraint", 1, 1)
-		expected_equation = ":ProductStack1CurrentColor_ID = :RawCylinderColor_ID or :ProductStack2CurrentColor_ID = :RawCylinderColor_ID or :ProductStack3CurrentColor_ID = :RawCylinderColor_ID"
-		print(equation)
-		assert(equation == expected_equation)
-
-
-t = TestThreeCaps()
-t.test_create_and_solve()
+		expected_equation = "|http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#ProductStack1CurrentColor_DE_1_1| = |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#RawCylinderColor_DE_1_1| or |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#ProductStack2CurrentColor_DE_1_1| = |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#RawCylinderColor_DE_1_1| or |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#ProductStack3CurrentColor_DE_1_1| = |http://www.hsu-hh.de/aut/ontologies/lab/MPS500/RawCylinderSupplyModule#RawCylinderColor_DE_1_1|"
+		assert expected_equation == equation
